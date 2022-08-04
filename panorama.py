daa8707b78549ef34bf42c0e9772d7111479359a
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2022-08-04 14:37:21 +0200

    simple commit to panorama

---
+++ panorama.py
@@ -0,0 +1,65 @@
+#!env python3
+import argparse
+import base64
+import botocore
+import boto3
+import copy
+import json
+from lxml import etree
+from lxml.builder import E
+import requests
+import re
+import sys
+
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+base_params = {
+    'key': '',
+    'type': 'op',
+}
+pano_base_url = 'https://{}/api/'.format('dummy')
+
+
+def readConfiguration():
+    global pano_base_url
+    with open("/Users/rweglarz/prog/ce-common/panorama_creds.json") as f:
+        data = json.load(f)
+        base_params["key"] = data["api_key"]
+        pano_base_url = 'https://{}/api/'.format(data['hostname'])
+
+
+def panorama_commit():
+    params = copy.copy(base_params)
+    params['type'] = 'commit'
+    r = etree.Element('commit')
+    params['cmd'] = etree.tostring(r)
+    resp = requests.get(pano_base_url, params=params, verify=False).content
+    xml_resp = etree.fromstring(resp)
+    if not xml_resp.attrib.get('status') == 'success':
+        print("Failed submit commit")
+        print(resp)
+        return False
+    msg = xml_resp.find('.//msg').text
+    if msg == "There are no changes to commit.":
+        print(msg)
+        return True
+    job = xml_resp.find('.//job').text
+    print('job id: {}'.format(job))
+    return True
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='useful actions on panorama'
+    )
+    #parser.add_argument('--clean', action='store_true')
+    args = parser.parse_args()
+
+    readConfiguration()
+    panorama_commit()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
