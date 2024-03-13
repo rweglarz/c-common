c1b9ee9a3e1f6f5b37fa0af016a3446133ee9a0a
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2024-03-13 10:07:11 +0100

    feat: tfscale: dynamically generate rules

--- tfscale/tfscale.py
+++ tfscale/tfscale.py
@@ -9,6 +9,20 @@ import sys
 import time
 
 
+def security_rule(name):
+  return {
+    "name": name,
+    "source_zones": ["any"],
+    "source_addresses": ["any"],
+    "source_users": ["any"],
+    "destination_zones": ["any"],
+    "destination_addresses": ["any"],
+    "applications": ["any"],
+    "services": ["application-default"],
+    "categories": ["any"],
+    "action": "allow",
+  }
+
 def generate_panos_security_rule_group():
   psrg = {
      "resource": {
@@ -24,24 +38,14 @@ def generate_panos_security_rule_group():
            "perf1": {
               "device_group": "${resource.panos_device_group.perf1.name}",
               "position_keyword": "top",
-              "rule": [
-                 {
-                    "name": "test1",
-                    "source_zones": ["any"],
-                    "source_addresses": ["any"],
-                    "source_users": ["any"],
-                    "destination_zones": ["any"],
-                    "destination_addresses": ["any"],
-                    "applications": ["any"],
-                    "services": ["application-default"],
-                    "categories": ["any"],
-                    "action": "allow",
-                 },
-              ],
+              "rule": []
            }
         }
       }
   }
+  for i in range(1,1000):
+    nsr = security_rule("dummy-{:04}".format(i))
+    psrg["resource"]["panos_security_rule_group"]["perf1"]["rule"].append(nsr)
   with open("panorama1.tf.json", "w") as tfj:
     tfj.write(json.dumps(psrg))
 
