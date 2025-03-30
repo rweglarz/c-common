f6a363aa0821d8f6ca9b8d0f47f803811cc41c54
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2025-03-30 08:09:22 +0200

    feat: scm-rules: create, delete rules and enforce order

--- scm-rules/scm-rules.py
+++ scm-rules/scm-rules.py
@@ -72,17 +72,100 @@ def buildRules(rules):
 
 
 
-def applyRules(client, rules):
-    for folder in rules:
-        print(folder)
-        for rt in ['pre', 'post']:
-            if rt in rules[folder]:
-                for r in rules[folder][rt]:
-                    name = r.get('name')
-                    try:
-                        client.security_rule.create(r, rulebase=rt)
-                    except scm.exceptions.NameNotUniqueError:
-                        print(f"Rule {name} already exists")
+def compareRuleExistenceInFolder(required_rules, existing_rules):
+    required_rules_names = [r.get('name') for r in required_rules]
+    existing_rules_names = [r.name for r in existing_rules]
+    rules_to_create = []
+    rules_to_delete = []
+    for rule_name in required_rules_names:
+        if rule_name not in existing_rules_names:
+            rules_to_create.append(rule_name)
+
+    for rule_name in existing_rules_names:
+        if rule_name not in required_rules_names:
+            rules_to_delete.append(rule_name)
+    return (rules_to_create, rules_to_delete)
+
+
+
+def planRuleMoves(required_rules, existing_rules):
+    rule_ids = {}
+    for r in existing_rules:
+        rule_ids[r.name] = r.id
+    required_rules_names = [r.get('name') for r in required_rules]
+    existing_rules_names = [r.name for r in existing_rules]
+
+    operations = []
+    for (idx, rule_name) in enumerate(required_rules_names):
+        ex_idx = existing_rules_names.index(rule_name)
+        if idx==ex_idx:
+            print(f"  Rule {rule_name} in correct position")
+            continue
+        print(f"  Move {rule_name} from {ex_idx=} to {idx=}")
+        if idx==0:
+            operations.append({
+                "rule_id": rule_ids[rule_name],
+                "destination": "top",
+                "destination_rule": None,
+            })
+        else:
+            operations.append({
+                "rule_id": rule_ids[rule_name],
+                "destination": "before",
+                "destination_rule": rule_ids[existing_rules_names[idx]]
+            })
+        existing_rules_names.pop(ex_idx)
+        existing_rules_names.insert(idx, rule_name)
+    return operations
+
+
+
+def applyRuleSetToSCMFolder(folder, complete_rules):
+    client = Scm(
+        client_id=base_params['sa_id'],
+        client_secret=base_params['sa_pass'],
+        tsg_id=base_params['tsg_id']
+    )
+    required_rules = complete_rules[folder][rt]
+    existing_rules = client.security_rule.list(folder=folder, rulebase=rt, exact_match=True)
+    (rules_to_create, rules_to_delete) = compareRuleExistenceInFolder(required_rules, existing_rules)
+    print(f" {rules_to_create=}")
+    print(f" {rules_to_delete=}")
+
+    required_rules_names = [r.get('name') for r in required_rules]
+    existing_rules_names = [r.name for r in existing_rules]
+    modified_ruleset = False
+
+    print(" Creating...")
+    for r in rules_to_create:
+        idx = required_rules_names.index(r)
+        rule = required_rules[idx]
+        print(f"  create {r=}")
+        client.security_rule.create(rule, rulebase=rt)
+        modified_ruleset = True
+    print(" Deleting...")
+    for r in rules_to_delete:
+        idx = existing_rules_names.index(r)
+        id = existing_rules[idx].id
+        print(f"  delete {r=} {id=}")
+        client.security_rule.delete(id, rulebase=rt)
+        modified_ruleset = True
+
+    if modified_ruleset:
+        print("Refreshing rules")
+        existing_rules = client.security_rule.list(folder=folder, rulebase=rt, exact_match=True)
+
+    print(" Rule order check...")
+    moves = planRuleMoves(required_rules, existing_rules)
+    for move in moves:
+        rule_id = move['rule_id']
+        move_target = {
+            "destination": move['destination'],
+            "destination_rule": move['destination_rule'],
+            "rulebase": rt
+        }
+        client.security_rule.move(rule_id, move_target)
+
 
 
 if __name__ == "__main__":
@@ -91,14 +174,12 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     readConfiguration()
+    print(f"Using file {args.file}")
     rules_from_file = readRules(args.file)
     complete_rules = buildRules(rules_from_file)
-    print(complete_rules)
 
-    client = Scm(
-        client_id=base_params['sa_id'],
-        client_secret=base_params['sa_pass'],
-        tsg_id=base_params['tsg_id']
-    )
-    applyRules(client, complete_rules)
+    for folder in rules_from_file:
+        for rt in ['pre', 'post']:
+            print(f"Halding {folder} {rt}")
+            applyRuleSetToSCMFolder(folder, complete_rules)
 
